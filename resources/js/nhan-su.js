49ba002182d47866976c2ab976edf8e9aebb8620
ductrieu2910 <trieu0892@gmail.com>
2024-03-20 17:06:45 +0700

    feat: nhan-su.html

---
+++ resources/js/nhan-su.js
@@ -0,0 +1,13 @@
+// Trạng Thái 
+document.getElementById("mydot").addEventListener("click", function() {
+this.classList.toggle("green");
+});
+document.getElementById("mydot1").addEventListener("click", function() {
+this.classList.toggle("green");
+});
+document.getElementById("mydot2").addEventListener("click", function() {
+this.classList.toggle("green");
+});
+document.getElementById("mydot3").addEventListener("click", function() {
+this.classList.toggle("green");
+});
