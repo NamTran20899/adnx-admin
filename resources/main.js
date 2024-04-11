b3040d7e233fc4a3cf6260a95a62e538ba376456
ductrieu2910 <trieu0892@gmail.com>
2024-04-11 09:14:55 +0700

    no message

--- resources/main.js
+++ resources/main.js
@@ -257,6 +257,28 @@ $(document).ready(function () {
   });
 });
 
+var popovers = document.querySelectorAll('.popover');
+var popoverTriggers = document.querySelectorAll('.popover__trigger');
+
+for (var i = 0; i < popoverTriggers.length; i++) {
+	popoverTriggers[i].addEventListener('click', function(event) {
+		closeAllOthers(this.parentElement);
+		this.parentElement.classList.toggle('popover--active');
+	});
+}
+
+function closeAllOthers(ignore) {
+	for (var i = 0; i < popovers.length; i++) {
+		if ( popovers[i] !== ignore) {
+			popovers[i].classList.remove('popover--active');	
+		}
+	}
+}
+
+
+
+
+
 // Flag
 var flagColors = ["red", "yellow", "green"];
 //
